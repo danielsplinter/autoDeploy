452df4611e254f913407ccd5d50b95994e57e375
r2d2 <r2d2@r2d2.local>
2025-05-13 22:31:54 -0300

    [MANAGE BUILD] reconhecimento do OS para maven

--- src/main/java/br/com/automate/deploy/processos/ManageBuild.java
+++ src/main/java/br/com/automate/deploy/processos/ManageBuild.java
@@ -24,22 +24,38 @@ public class ManageBuild {
     }
 
     public void executeBuild(String modulos){
-        String comandoMavenMontadoModulosAlterados = "cmd /c mvn clean install -pl "+modulos+" -o -DskipTests";//teste
-        String comandoMavenModuloEAR = "cmd /c mvn clean install -o -DskipTests";//teste
+        // Obtendo o nome do sistema operacional
+        String os = System.getProperty("os.name").toLowerCase();
+
+        String comandoMavenMontadoModulosAlterados = "mvn clean install -pl " + modulos + " -o -DskipTests";
+        String comandoMavenModuloEAR = "mvn clean install -o -DskipTests";
+
+        // Caso esteja no Windows, adiciona o 'cmd /c' antes do comando Maven
+        if (os.contains("win")) {
+            comandoMavenMontadoModulosAlterados = "cmd /c " + comandoMavenMontadoModulosAlterados;
+            comandoMavenModuloEAR = "cmd /c " + comandoMavenModuloEAR;
+        }
+
         String projectFolder = configManager.getConfiguracoesDTO().getProjectPerfils().get(0).getBuildConfigDTO().getProjectFolder();
         String earFolder = configManager.getConfiguracoesDTO().getProjectPerfils().get(0).getBuildConfigDTO().getModuloFinalEAR();
 
+        // Converte o comando em uma lista de parâmetros
         List<String> comandoMavenModulosAlteradosParametro = Arrays.stream(comandoMavenMontadoModulosAlterados.split(" ")).collect(Collectors.toList());
 
+        // Executando o build do módulo alterado
         processoBuild.setDirectoryExecute(new File(projectFolder));
-        int statusExitCode =  processoBuild.execute(comandoMavenModulosAlteradosParametro);
+        int statusExitCode = processoBuild.execute(comandoMavenModulosAlteradosParametro);
 
-        if(statusExitCode == 0){
+        if (statusExitCode == 0) {
+            // Converte o comando para o EAR em uma lista de parâmetros
             List<String> comandoMavenEARParametros = Arrays.stream(comandoMavenModuloEAR.split(" ")).collect(Collectors.toList());
-            processoBuild.setDirectoryExecute(new File(projectFolder+earFolder));
+
+            // Executando o build do EAR
+            processoBuild.setDirectoryExecute(new File(projectFolder + earFolder));
             processoBuild.execute(comandoMavenEARParametros);
         }
     }
+
 
     public void executeDeploy(String modulo){
         //String comandoMavenMontado = getConfiguracoes().getConfiguracao().get(2)+" clean package install -f "+getConfiguracoes().getConfiguracao().get(1)+"\\"+modulo+"\\pom.xml -DskipTests";
